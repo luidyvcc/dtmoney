e074a43d0f0dc0d017a302bc42429bf97026290e
Luidy Vaccaro <luidy@upoutsourcing.com>
2021-03-20 10:55:56 -0300

    first commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: var(--main);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 2rem 1rem 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    color: #fff;
+    background: var(--main-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.2s;
+
+    :hover>* {
+      filter: brightness(0.9);
+      filter: blur(1.2px);
+    }
+  }
+`;
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { useTransactions } from '../../hooks/useTransactions';
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  const { transactions } = useTransactions();
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map(item => (
+            <tr key={item.id}>
+              <td>{item.title}</td>
+              <td className={item.type}>
+                {item.type === 'withdraw' && '-'}
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(item.amount)}
+              </td>
+              <td>{item.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(item.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
+}
